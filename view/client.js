270f9f327a912278d360270eb7b8f95d3535c8f8
christ valtes medjouwo diagong <christ.medjoumo@studio.unibo.it>
2020-12-08 14:29:53 +0100

    --- Auto-Git Commit ---

--- view/client.js
+++ view/client.js
@@ -100,4 +100,26 @@ function deleteId() {
     options.error = function (err) { alert(err.responseText); };
     $.ajax(options);
     };
+
+    function SelectSerie() {
+      var id = $("#txtId").val();
+      $.ajax({
+        url: "https://localhost:5001/api/Indici/" + id,
+        type: "GET",
+        contentType: "application/json",
+        data: "",
+        success: function (result) {
+          console.log(result);
+          showResult(JSON.parse(result));
+          //readResult(JSON.stringify(result));
+        },
+        error: function (xhr, status, p3, p4) {
+          var err = "Error " + " " + status + " " + p3;
+          if (xhr.responseText && xhr.responseText[0] == "{")
+            err = JSON.parse(xhr.responseText).message;
+          alert(err);
+        },
+      });
+     
+   };
   
