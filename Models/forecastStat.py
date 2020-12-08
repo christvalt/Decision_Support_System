c7d53afd0e0e73844cd28493788bfe7806ac1bfa
christ valtes medjouwo diagong <christ.medjoumo@studio.unibo.it>
2020-12-08 10:59:02 +0100

    --- Auto-Git Commit ---

--- Models/forecastStat.py
+++ Models/forecastStat.py
@@ -1,13 +1,7 @@
 import numpy as np, pandas as pd
 import matplotlib.pyplot as plt
 import os, sys, io, base64
-import pandas as pd, matplotlib.pyplot as plt , numpy as np
-from statsmodels.tsa.seasonal import seasonal_decompose
-'''
-prendi la mia seria la si lavora e genera una lunga stinga che contiene una imagine 
 
-******nella cartella server dentro model devo fare python forcastStat.py seriea_storica.csv  
-'''
 def print_figure(fig):
 	"""
 	Converts a figure (as created e.g. with matplotlib or seaborn) to a png image and this 
@@ -28,23 +22,63 @@ if __name__ == "__main__":
    print('MAPE Argument List:', str(sys.argv), ' first true arg:',sys.argv[1])   
    
    dffile = sys.argv[1]
-   df = pd.read_csv("C:/Users/camerum/Desktop/Decision_Support_system/SsdWebApi\Models/"+dffile)
-   #to see all the component present in my serie 
-   result = seasonal_decompose(df, model='multiplicative',freq=4)
-   result.plot()
-   #Preprocessing: log transform
+   df = pd.read_csv("C:/Users/camerum/Desktop/Decision_Support_system/SsdWebApi/Models/"+dffile)
+       #Preprocessing: log transform
    npa = df['All_Bonds'].to_numpy()
    logdata = np.log(npa)
    logdata=pd.Series(logdata)
-    
    difflog = logdata.diff()
    difflog=difflog[1:]
-
    plt.plot(df)
+   #slpit dei dati
+   cutpoint = int(0.9*len(difflog))
+   train = difflog[:cutpoint]
+   test = difflog[cutpoint:]
+   
+   #another corelation more good
+   import statsmodels.api as sm
+   sm.graphics.tsa.plot_acf(df, lags=110)
+   
+   
+#Sarimax model  
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+sarima_model = SARIMAX(train, order=(1,0,1), seasonal_order=(0,1,1,5))
+sfit = sarima_model.fit()
+sfit.plot_diagnostics(figsize=(10, 6))
+plt.show()
+#print(sarima_model.summary())
+
+#dati di predicton non di forcast ancora detto prediction in sample
+
+ypred=sfit.predict(start=0,end=len(train))
+plt.plot(train)
+plt.plot(ypred)
+plt.title("trian")
+
+#previsione de dati quindi il forcat --->il modelo è stato esteso al futuro
+forewrap = sfit.get_forecast(steps=523)
+forecast_ci = forewrap.conf_int()
+forecast_val = forewrap.predicted_mean
+#forecast_val=forecast_val[1:]
+#plt.plot(train)
+#plt.fill_between(forecast_ci.index,forecast_ci.iloc[:, 0],forecast_ci.iloc[:, 1], color='k', alpha=.25)
+plt.plot(forecast_val)
+#plt.plot(test)
+plt.show()
+
+# Accuracy metrics
+def forecast_accuracy(forecast_val, test):
+
+    rmse = np.mean((forecast_val - test)**2)**.523 # RMSE
+    return({ 'rmse':rmse})
+print( forecast_accuracy(forecast_val, test) )
+   #sm.graphics.tsa.plot_acf(train.values, lags=100)
+plt.show
+
    #plt.show()
    
    # Finally, print the chart as base64 string to the console.
-   print_figure(plt.gcf())
+print_figure(plt.gcf())
    
 
    
