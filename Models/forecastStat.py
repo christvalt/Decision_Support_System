9d9ecb87dcac59fb8edfa76fd8b9a0b9f6e8dc21
christ valtes medjouwo diagong <christ.medjoumo@studio.unibo.it>
2020-12-13 18:48:27 +0100

    --- Auto-Git Commit ---

--- Models/forecastStat.py
+++ Models/forecastStat.py
@@ -32,7 +32,7 @@ if __name__ == "__main__":
    
    dffile = sys.argv[1]
    df = pd.read_csv("C:/Users/camerum/Desktop/Decision_Support_system/SsdWebApi/Models/"+dffile , usecols=['All_Bonds'], names=['All_Bonds'],header=0)
-   df.plot()  
+   #df.plot()  
    #Preprocessing: log transform
    npa = df['All_Bonds'].to_numpy()
    logdata = np.log(npa)
@@ -45,9 +45,11 @@ if __name__ == "__main__":
    train = difflog[:cutpoint]
    test = difflog[cutpoint:]
    
+'''
    #another corelation more good
    import statsmodels.api as sm
    sm.graphics.tsa.plot_acf(df, lags=500)
+'''
    
    
 #Sarimax model  
@@ -55,27 +57,27 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 sarima_model = SARIMAX(train, order=(1,0,1), seasonal_order=(0,1,1,2), enforce_stationarity=False, enforce_invertibility=False)
 sfit = sarima_model.fit()
 print(sfit.summary())
-sfit.plot_diagnostics(figsize=(10, 6))
-plt.show()
+#sfit.plot_diagnostics(figsize=(10, 6))
+#plt.show()
 #print(sarima_model.summary())
 
 #dati di predicton non di forcast ancora detto prediction in sample
 
 ypred=sfit.predict(start=0,end=len(train))
-plt.plot(train)
-plt.plot(ypred)
-plt.title("trian")
+#plt.plot(train)
+#plt.plot(ypred)
+#plt.title("trian")
 
 #previsione de dati quindi il forcat --->il modelo è stato esteso al futuro
 forewrap = sfit.get_forecast(steps=523)
 forecast_ci = forewrap.conf_int()
 forecast_val = forewrap.predicted_mean
 #forecast_val=forecast_val[1:]
-plt.plot(train)
-plt.fill_between(forecast_ci.index,forecast_ci.iloc[:, 0],forecast_ci.iloc[:, 1], color='red', alpha=.25)
-plt.plot(forecast_val)
-plt.plot(test)
-plt.show()
+#plt.plot(train)
+#plt.fill_between(forecast_ci.index,forecast_ci.iloc[:, 0],forecast_ci.iloc[:, 1], color='red', alpha=.25)
+#plt.plot(forecast_val)
+#plt.plot(test)
+#plt.show()
 
 # Accuracy metrics
 def forecast_accuracy(forecast_val, test):
@@ -92,14 +94,14 @@ ypred = pd.Series(ypred)
 expdata = np.exp(ypred) # unlog
 plt.plot(expdata)
 expfore = np.exp(forecast_val)
-plt.plot(forecast_val)
-plt.plot([None for x in range(12)]+[x for x in expdata[12:]])
-plt.plot(df)
-plt.plot(reconstruct)
-plt.plot([None for x in expdata]+[x for x in expfore])
+#plt.plot(forecast_val)
+#plt.plot([None for x in range(12)]+[x for x in expdata[12:]])
+#plt.plot(df)
+#plt.plot(reconstruct)
+#plt.plot([None for x in expdata]+[x for x in expfore])
 print('Actual predict:', forecast_val)   
 
-plt.show
+#plt.show
 
 plt.plot(forecast_val)
 plt.show()
