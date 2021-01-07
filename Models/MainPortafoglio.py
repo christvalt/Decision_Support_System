fd6e2f36bfd3fa88106cdf1dea1c6e41b4e45a62
christ valtes medjouwo diagong <christ.medjoumo@studio.unibo.it>
2021-01-07 11:20:09 +0100

    --- Auto-Git Commit ---

--- Models/MainPortafoglio.py
+++ Models/MainPortafoglio.py
@@ -59,17 +59,19 @@ def forecast(id):
     print(sfit.summary())
     sfit.plot_diagnostics(figsize=(10, 6))
     #plt.show()
-  
+    index_forecasts = pd.Series(range(df.index[-1] + 1 - horizon_data_length, df.index[-1] + 1))
+   
     # Predictions of y values based on "model", aka fitted values
-    #index_forecasts = pd.Series(range(df.index[-1] + 1 - horizon_data_length, df.index[-1] + 1))
+    index_forecasts = pd.Series(range(df.index[-1] + 1 - horizon_data_length, df.index[-1] + 1))
     ypred=sfit.predict(start=0,end=len(train))
-    forewrap = sfit.get_forecast(steps=horizon_data_length)
+    forewrap= sfit.get_forecast(steps=horizon_data_length)
     #intervalo di forcast interessante
     forecast_ci = forewrap.conf_int()
     forecast_val = forewrap.predicted_mean
     forecast_val=forecast_val[1:]
-    forecast_ci=pd.Series(forecast_ci)
-
+    forecast_ci=pd.Series(forecast_val[1:], index=index_forecasts)
+    re=pd.Series(range)
+    
     metrics = forecast_accuracy(forecast_val, test)
     print("RMSE is "+id,metrics['rmse'])
 
@@ -77,9 +79,7 @@ def forecast(id):
     for j in range(0, horizon_data_length):
         print("Actual {} forcast {:.2f}".format(id,forecast_val[j-1]))
         yfore.append(forecast_val[j-1])
-        #  Postprocessing, reconstruction
-        train[0] = 0
-        reconstruct = np.exp(np.r_[train,test].cumsum()+logdata[0])
+       
         
     expdata = np.exp(ypred) # unlog
     expfore = np.exp(yfore)
